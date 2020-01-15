63ec3b44f2d16bacc8a59c5289cb3ded568a07c5
Niklas Kreer <kreergit@posteo.de>
2020-01-15 17:51:40 +0100

    Improve request text and styling

--- generator.js
+++ generator.js
@@ -7,12 +7,12 @@ I hereby request a full copy of the personal data your company processes and sto
 
 - the purposes of processing
 - the categories of personal data concerned;
-- the recipients or categories of recipient to whom the personal data have been or will be disclosed, in particular recipients in third countries or international organisations;
-- where possible, the envisaged period for which the personal data will be stored, or, if not possible, the criteria used to determine that period;
+- the recipients or categories of recipient to whom my personal data have been or will be disclosed, in particular recipients in third countries or international organisations;
+- where possible, the envisaged period for which my personal data will be stored, or, if not possible, the criteria used to determine that period;
 - the existence of the right to request from the controller rectification or erasure of personal data or restriction of processing of personal data concerning the data subject or to object to such processing;
 - the right to lodge a complaint with a supervisory authority;
-- where the personal data are not collected from the data subject, any available information as to their source;
-- the existence of automated decision-making, including profiling, referred to in Article 22(1) and (4) and, at least in those cases, meaningful information about the logic involved, as well as the significance and the envisaged consequences of such processing for the data subject.
+- where the personal data are not collected from myself, any available information as to their source;
+- the existence of automated decision-making, including profiling, referred to in Article 22(1) and (4) of the GDPR and, at least in those cases, meaningful information about the logic involved, as well as the significance and the envisaged consequences of such processing for myself.
 
 As this is a request conforming to article 15 of the EU-GDPR, it must be granted for free, within one month time. {{proof}}
 
@@ -36,4 +36,8 @@ function generate(){
 	}
 
 	document.getElementById("generatedText").innerHTML = text;
+
+	// Make requested text visible
+	document.getElementById("request").style.display = "block";
+	document.getElementById("request").scrollIntoView();
 }
